c0140814d707428a8b203dfc629c922ee028af81
Sebastian Reimers <hallo@studio-link.de>
2020-01-03 13:27:35 +0100

    fix verify

--- win.cmd
+++ win.cmd
@@ -1,4 +1,4 @@
 curl -o standalone.exe https://download.studio.link/nightly/v19.xx.x/v19.12.0-alpha-774.25e7c40/windows64/studio-link-standalone-v19.12.0.exe
 "%SignTool%" sign /fd SHA256 /a /f certificate.pfx /p %pfx_password% /tr http://time.certum.pl standalone.exe
-"%SignTool%" verify /pb standalone.exe
+"%SignTool%" verify /pa standalone.exe
 appveyor PushArtifact standalone.exe
