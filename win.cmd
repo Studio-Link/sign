ea765e740cdf4d0f3381b2f9cd54b4fa7a2ff7d1
Sebastian Reimers <hallo@studio-link.de>
2020-01-03 14:05:59 +0100

    fix version

--- win.cmd
+++ win.cmd
@@ -1,5 +1,5 @@
-curl -o standalone.exe https://download.studio.link/releases/%APPVEYOR_REPO_TAG_NAME%/windows64/studio-link-standalone-windows64-%APPVEYOR_REPO_TAG_NAME%.exe
+curl -o standalone.exe https://download.studio.link/releases/%APPVEYOR_REPO_TAG_NAME%/windows64/studio-link-standalone-%APPVEYOR_REPO_TAG_NAME%.exe
 "%SignTool%" sign /fd SHA256 /a /f certificate.pfx /p %pfx_password% /tr http://time.certum.pl standalone.exe
-copy standalone.exe studio-link-standalone-windows64-signed-%APPVEYOR_REPO_TAG_NAME%.exe
-"%SignTool%" verify /pa studio-link-standalone-windows64-signed-%APPVEYOR_REPO_TAG_NAME%.exe
-appveyor PushArtifact studio-link-standalone-windows64-signed-%APPVEYOR_REPO_TAG_NAME%.exe
+copy standalone.exe studio-link-standalone-signed-%APPVEYOR_REPO_TAG_NAME%.exe
+"%SignTool%" verify /pa studio-link-standalone-signed-%APPVEYOR_REPO_TAG_NAME%.exe
+appveyor PushArtifact studio-link-standalone-signed-%APPVEYOR_REPO_TAG_NAME%.exe
