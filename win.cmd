954d9a5843d428d4e8318b6aa930fb5fb30bceda
Sebastian Reimers <hallo@studio-link.de>
2020-01-03 14:01:16 +0100

    fix download

--- win.cmd
+++ win.cmd
@@ -1,5 +1,5 @@
-curl -o standalone.exe https://download.studio.link/nightly/v19.xx.x/v19.12.0-alpha-774.25e7c40/windows64/studio-link-standalone-v19.12.0.exe
-curl -o standalone.exe https://download.studio.link/releases/%APPVEYOR_REPO_TAG_NAME%/studio-link-standalone-windows64.zip
+curl -o standalone.exe https://download.studio.link/releases/%APPVEYOR_REPO_TAG_NAME%/windows64/studio-link-standalone-windows64-%APPVEYOR_REPO_TAG_NAME%.exe
 "%SignTool%" sign /fd SHA256 /a /f certificate.pfx /p %pfx_password% /tr http://time.certum.pl standalone.exe
-"%SignTool%" verify /pa standalone.exe
-appveyor PushArtifact standalone.exe
+copy standalone.exe studio-link-standalone-windows64-signed-%APPVEYOR_REPO_TAG_NAME%.exe
+"%SignTool%" verify /pa studio-link-standalone-windows64-signed-%APPVEYOR_REPO_TAG_NAME%.exe
+appveyor PushArtifact studio-link-standalone-windows64-signed-%APPVEYOR_REPO_TAG_NAME%.exe
